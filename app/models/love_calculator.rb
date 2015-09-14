f5a26e693c171e15012aac673cd880591e4d851d
Cara Hill <chilly23@gmail.com>
2015-09-14 16:02:16 +1200

    Trying to make this work.

---
+++ app/models/love_calculator.rb
@@ -0,0 +1,2 @@
+class LoveCalculator < ActiveRecord::Base
+end
---
+++ test/models/love_calculator_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LoveCalculatorTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
